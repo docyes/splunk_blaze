0eddf74e7dd7084bda9cbdff0189a28b2d56c46e
Doc Yes <docyes@16watts.com>
2009-09-28 15:56:02 -0700

    Shaved off 100ms. request response time. Expanded cli options. Switch to GET for search requests in order for future browser cache support. Trim some old files.

--- splunkblaze.py
+++ splunkblaze.py
@@ -2,8 +2,10 @@
 
 import os.path
 import time
-import splunk.auth
-import splunk.search
+import httplib2
+import urllib
+import io
+import lxml.etree as et
 import tornado.httpserver
 import tornado.ioloop
 import tornado.options
@@ -13,9 +15,14 @@ import uimodules
 from tornado.options import define, options
 
 define("port", default=8888, help="web server port", type=int)
+define("debug", default=False, help="web server debug mode", type=bool)
 define("splunk_host_path", default="https://localhost:8089", help="splunk server scheme://host:port (Use http over https for performance bump!)")
-define("splunk_username", default="admin", help="splunk user")
+define("splunk_username", default="admin", help="splunk username")
 define("splunk_password", default="changeme", help="splunk password")
+define("splunk_search_segmentation", default="outer", help="splunk search segmentation, one of inner, outer, full, raw", type=str)
+define("splunk_search_spawn_process", default=False, help="splunk search spawns new process", type=bool)
+define("splunk_search_query_prefix", default="search index=_* ", help="splunk search query prefix", type=str)
+define("splunk_search_query_suffix", default="* | fields | head 10", help="splunk search query suffix", type=str)
 
 class Application(tornado.web.Application):
     def __init__(self):
@@ -28,69 +35,47 @@ class Application(tornado.web.Application):
             static_path=os.path.join(os.path.dirname(__file__), "static"),
             cookie_secret="e220cf903f537500f6cfcaccd64df14d",
             xsrf_cookies=True,
-            ui_modules=uimodules,            
+            ui_modules=uimodules,
+            splunk_host_path=options.splunk_host_path,
+            splunk_username=options.splunk_username,
+            splunk_password=options.splunk_password,
+            splunk_search_segmentation=options.splunk_search_segmentation,
+            splunk_search_spawn_process=options.splunk_search_spawn_process,
+            splunk_search_query_prefix=options.splunk_search_query_prefix,
+            splunk_search_query_suffix=options.splunk_search_query_suffix,
+            debug=options.debug,
         )
         tornado.web.Application.__init__(self, handlers, **settings)
-        
-        #Have one global splunk session_key accross all users.
-        self.session_key = splunk.auth.getSessionKey(options.splunk_username, options.splunk_password, hostPath=options.splunk_host_path)
+        #one global raw transformer
+        xslt_file = io.open(os.path.join(settings['template_path'], "raw.xslt"), encoding="utf-8")
+        self.xslt_transform = et.XSLT(et.XML("".join(xslt_file.readlines())))
         
 class BaseHandler(tornado.web.RequestHandler):
-    @property
-    def session_key(self):
-        return self.application.session_key
+    pass
     
 class HomeHandler(BaseHandler):
     def get(self):
-        self.render("index.html")
+        self.render("index.html", splunk_search_query_prefix=self.settings.get("splunk_search_query_prefix"), splunk_search_query_suffix=self.settings.get("splunk_search_query_suffix"))
 
 class SearchHandler(BaseHandler):
-    def post(self):
-        job = splunk.search.dispatch(self.get_argument("search"), sessionKey=self.session_key, hostPath=options.splunk_host_path)
-        job.setFetchOption(
-            segmentationMode='full',
-            maxLines=500,
+    def get(self):
+        h = httplib2.Http(".cache", timeout=1)
+        h.add_credentials(self.settings.get('splunk_username'), self.settings.get('splunk_password'))
+        data = dict(
+            search=self.get_argument("search"),
+            spawn_process="1" if options.splunk_search_spawn_process else "0",
+            segmentation=options.splunk_search_segmentation,
         )
-        maxtime = 1
-        pause = 0.05
-        lapsed = 0.0
-        while not job.isDone:
-            time.sleep(pause)
-            lapsed += pause
-            if maxtime >= 0 and lapsed > maxtime:
-                break
-        xslt = '''<?xml version="1.0" encoding="UTF-8"?>
-        <xsl:stylesheet version="1.0" xmlns:xsl="http://www.w3.org/1999/XSL/Transform">
-            <xsl:strip-space elements="*" />
-            <xsl:preserve-space elements="v sg" />
-            <xsl:output method="html" indent="no" />
-            <xsl:template match="/">
-                <xsl:apply-templates select="v" />
-            </xsl:template>
-            <xsl:template match="v">
-                <xsl:apply-templates />
-            </xsl:template>
-            <xsl:template match="sg">
-                <em>
-                    <xsl:attribute name="class">
-                        <xsl:text>t</xsl:text>
-                        <xsl:if test="@h">
-                            <xsl:text> a</xsl:text>
-                        </xsl:if>
-                    </xsl:attribute>
-                    <xsl:apply-templates />
-                </em>
-            </xsl:template>
-        </xsl:stylesheet>
-        '''
-        self.render("search.html", job=job, xslt=xslt)
+        resource = "%s/services/search/jobs/oneshot?%s" % (self.settings.get("splunk_host_path"), urllib.urlencode(data))
+        resp, content = h.request(resource)
+        xml_doc = et.fromstring(content)
+        self.render("search.html", xml_doc=xml_doc, xslt_transform=self.application.xslt_transform, search=self.get_argument("search"))
 
 def main():
     tornado.options.parse_command_line()
     http_server = tornado.httpserver.HTTPServer(Application())
     http_server.listen(options.port)
-    tornado.ioloop.IOLoop.instance().start()
- 
+    tornado.ioloop.IOLoop.instance().start() 
  
 if __name__ == "__main__":
     main()
---
+++ templates/raw.xslt
@@ -0,0 +1,23 @@
+<!-- <?xml version="1.0" encoding="UTF-8"?>  -->
+<xsl:stylesheet version="1.0" xmlns:xsl="http://www.w3.org/1999/XSL/Transform">
+    <xsl:strip-space elements="*" />
+    <xsl:preserve-space elements="v sg" />
+    <xsl:output method="html" indent="no" />
+    <xsl:template match="/">
+        <xsl:apply-templates select="v" />
+    </xsl:template>
+    <xsl:template match="v">
+        <xsl:apply-templates />
+    </xsl:template>
+    <xsl:template match="sg">
+        <em>
+            <xsl:attribute name="class">
+                <xsl:text>t</xsl:text>
+                <xsl:if test="@h">
+                    <xsl:text> a</xsl:text>
+                </xsl:if>
+            </xsl:attribute>
+            <xsl:apply-templates />
+        </em>
+    </xsl:template>
+</xsl:stylesheet>
