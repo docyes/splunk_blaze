3d7715c5a24ea824da3f6ef380968b45550d81e7
Doc Yes <docyes@16watts.com>
2010-02-27 14:49:33 -0800

    Update ezsearch docs.

--- splunkblaze/ezsearch.py
+++ splunkblaze/ezsearch.py
@@ -1,5 +1,26 @@
 import sys,re
 
+"""
+~news   ------>  news OR "flash:" OR "breaking:"
+// middle east
+~me      -------> iraq* OR iran* OR syria* OR arabia* OR leban* OR israel* OR palestin*
+~movies  ------> movie* OR film* OR cinema*
+~tv ---------> tv OR  television OR t.v.
+~video     -----> hulu OR netflix OR youtube
+ // this would be a massive search!  but now it's just ~media!!  i'm lovin it!
+~media      ------> ~video OR ~tv OR ~movies OR ~music
+~games ---------> xbox OR x-box OR wii OR ps2 OR ps-2 OR ps-3 OR ps3 OR nintendo OR ds
+
+now wildcards anything not quoted,
+handles quotes properly,
+replaces " ~" with eventtype=,
+replace " -" with "NOT "
+it's actually more sophisticated in that they can also be at beginning of the search and don't need a space in front.
+
+$ python ezsearch.py '~foo ~"foo" "baz" -"bart bag foo" ~barf'                                                                                                                          
+EZSEARCH:       ~foo ~"foo" "baz" -"bart bag foo" ~barf                                                                                                                                             
+FULLSEARCH:     eventtype=*foo* eventtype="foo" "baz" NOT "bart bag foo" eventtype=*barf*
+"""
 
 # tmp replacements for ~ T(ilda), - D(ash), * A(sterisk)
 # that user is unlikely to ever type.
