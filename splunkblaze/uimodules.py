d5f4cd64505a5cb13247dd0bbc22fcbef50a3d4b
Doc Yes <spam@featureblend.com>
2010-03-12 10:12:43 -0800

    Remove uimodule for cssselector, extract css, js for base and home/index templates as includes. Remove external request for base.css. Add keywords and description for home handler template.

--- splunkblaze/uimodules.py
+++ splunkblaze/uimodules.py
@@ -1,11 +1,6 @@
 import tornado.web
-import web
 
 class Event(tornado.web.UIModule):
     def render(self, event, display_time=True):
         return self.render_string("modules/event.html", event=event, xslt_transform=self.handler.xslt_transform, display_time=display_time)
 
-class CSSSelector(tornado.web.UIModule):
-    def render(self, root="body", class_name=None):
-        class_name = class_name if class_name else web.contextual_class_name(self.handler)
-        return "%s.%s" % (root, class_name)
