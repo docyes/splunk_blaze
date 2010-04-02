5a361a5d73abc3aba482505ddecdba4533ddb6e8
cyestrau <cyestrau@skidrow.(none)>
2010-04-01 20:22:24 -0700

    Tests and fixes for single character wild carding.

--- splunkblaze/ezsearch.py
+++ splunkblaze/ezsearch.py
@@ -42,12 +42,12 @@ def processMatches(regex, text, func):
 def deencode(text):
     return 
 
-def expand(search):
+def expand(search, termprefix='', termsuffix='*'):
+    search = search.strip()
 
     # change any *,~,- inside of quotes to some temp A,D,T representation
     search = processMatches('(".*?")', search, lambda x: x.replace("*", A).replace("-", D).replace("~", T))
-
-    search = re.sub('(^|\\s*)([^~-]\\S+)($|\\s*)', "\\1\\2*\\3",      search)
+    search = re.sub('(^|\\s*)([^~-]\\S*)($|\\s*)', "\\1%s\\2%s\\3" % (termprefix, termsuffix),      search)
     search = re.sub("(\\s*)~(\\S)",                "\\1eventtype=\\2", search)
     search = re.sub("(\\s*)-(\\S)",                "\\1NOT \\2",       search)
 
@@ -57,9 +57,38 @@ def expand(search):
     search = search.replace(A, "*").replace(D, "-").replace(T, "~").replace("**", "*")
     return search
 
+
+def testAndExit():
+    # make tests
+    # tests = [ "a", "aa", "*a* *b", " a ", " a a ", " bar bar ", "barbar", "foo ~bar", "~foo ~bar", '"~foo ~bar" ~foo ~bar', 'foo -bar "-baz -bat"']
+    # for test in tests:
+    #    print "'%s':'%s', " % (test, expand(test,'','*')),
+    # exit(1)
+    
+    testanswers_empty_star = {
+        'a':'a*',  'aa':'aa*',  '*a* *b':'*a* *b*',  ' a ':'a*',  ' a a ':'a* a*',  ' bar bar ':'bar* bar*',  'barbar':'barbar*',
+        'foo ~bar':'foo* eventtype=bar*',  '~foo ~bar':'eventtype=foo* eventtype=bar*',
+        '"~foo ~bar" ~foo ~bar':'"~foo ~bar" eventtype=foo* eventtype=bar*',  'foo -bar "-baz -bat"':'foo* NOT bar* "-baz -bat"', }
+    testanswers_star_star = {
+        'a':'*a*',  'aa':'*aa*',  '*a* *b':'*a* *b*',  ' a ':'*a*',  ' a a ':'*a* *a*',  ' bar bar ':'*bar* *bar*',  'barbar':'*barbar*',
+        'foo ~bar':'*foo* eventtype=*bar*',  '~foo ~bar':'eventtype=*foo* eventtype=*bar*',
+        '"~foo ~bar" ~foo ~bar':'"~foo ~bar" eventtype=*foo* eventtype=*bar*',  'foo -bar "-baz -bat"':'*foo* NOT *bar* "-baz -bat"', }
+    
+    for t,a in testanswers_empty_star.items():
+        e = expand(t, '', '*')
+        if a != e: print "Error on star suffix! Given '%s', expected '%s', but got '%s'!" % (t, a, e)
+
+    for t,a in testanswers_star_star.items():
+        e = expand(t, '*', '*')
+        if a != e: print "Error on star prefix/suffix! Given '%s', expected '%s', but got '%s'!" % (t, a, e)
+        
+    exit(0)
+
 if __name__ == "__main__":
 
     ezsearch = ' '.join(sys.argv[1:])
+    if ezsearch == "test":
+        testAndExit()
     fullsearch = expand(ezsearch)
     print "EZSEARCH:\t%s\nFULLSEARCH:\t%s" % (ezsearch, fullsearch)
-    
+
